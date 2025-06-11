dc020be61a4ca1c005c88090dd7d9970e0ae01b1
reg1z <21206554+reg1z@users.noreply.github.com>
2025-06-11 11:42:41 -0700

    Primary installation script

---
+++ install.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# **************************************************************
+# DEFAULT PACKAGES AND SETUP
+#
+# **************************************************************
+
+# List of packages to install
+packages=(
+    "gum"                       # cli tool for shell scripts
+    "uwsm"                      # universal wayland session manager
+    "base-devel"                # basic development tools
+    "git"                       # version control system
+    "networkmanager"
+    "network-manager-applet"
+    "firewalld"                 # firewall
+    "blueman"                   # bluetooth manager
+    "openssh"                   # ssh
+    "upower"                    # power metrics
+    "power-profiles-daemon"     # power management
+    "hyprland"
+    "hyprpaper"
+    "hypridle"
+    "hyprlock"
+    "xdg-desktop-portal-hyprland"
+    "hyprland-qt-support"
+    "hyprcursor"
+    "hyprpolkitagent"           # polkit agent
+    "wl-clipboard"
+    "cliphist"
+    "waybar"
+    "rofi-wayland"              # application launcher
+    "mako"                      # notification daemon
+    "keyd"
+    "pipewire"
+    "wireplumber"
+    "qt5-wayland"
+    "qt6-wayland"
+    "grim"                      # screenshot utility
+    "slurp"                     # region selection utility (for grim)
+    "loupe"                     # image viewer
+    "thunar"                    # file manager
+    "gvfs"                      # file system support for thunar
+    "neovim"                    # text editor
+    "tmux"                      # terminal multiplexer
+    "kitty"                     # terminal emulator
+    "firefox"                   # web browser
+    "vlc"                       # media player
+    "ttf-fira-sans"             # font
+    "ttf-fira-code"             # font
+    "ttf-firacode-nerd"         # font
+    "ttf-terminus-nerd"         # font
+)
+
+# Create necessary directories
+mkdir -p ~/.config
+mkdir -p ~/.config/hypr         # cfg directory for hyprland ecosystem
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/mako
+
+# Install required packages
+sudo pacman -S --needed ${packages[@]}
+
+
+
+# **************************************************************
+# USER CHOICES
+#
+# **************************************************************
+
+
+# --------------------------------------------------------------
+# Nvidia Drivers
+# --------------------------------------------------------------
+
+# Ask if user if they have an Nvidia GPU
+if gum confirm --default=false "Are you using Nvidia hardware and want to install the necessary drivers?"; then
+    source scripts/install-nvidia.sh
+fi
+
+
+# --------------------------------------------------------------
+# AUR Helper
+# --------------------------------------------------------------
+
+# Ask if user wants to install AUR helper (yay) using gum with a default of no 
+if gum confirm --default=false "Do you want to install an AUR helper (yay/paru)?"; then
+    source scripts/install-aur-helper.sh
+fi
+
+
+# --------------------------------------------------------------
+# Keyboard
+# --------------------------------------------------------------
+
+# Explain keyd
+cat << EOM
+
+keyd is a keyboard daemon that allows you to remap your keyboard keys.
+Included is a default configuration that implements the Enthium layout
++ homerow mods for thinkpad keyboards.
+
+If you're unfamiliar with keyd, you will want to skip this.
+
+EOM
+
+# Ask if user wants to install keyd using gum with a default of no
+if gum confirm --default=false "Do you want to install keyd and the Enthium layout? (only recommended for keyboard enthusiasts)"; then
+    source ./scripts/keyd/install-keyd.sh
+fi
+
+
+# Copy configuration files
+cp -r ./.config/* ~/.config/
+
+# Copy wallpapers
+cp -r ./assets/wallpapers/ ~/Pictures/wallpapers/
+
+# enable services
+sudo systemctl --quiet enable --now NetworkManager
+sudo systemctl --quiet enable --now firewalld
+sudo systemctl --quiet enable --now power-profiles-daemon
+
+echo "Installation complete! Please log out and log back in to start using Hyprland." 
