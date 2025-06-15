2ec836cca7100b569845963feb34193ac08c747c
reg1z <21206554+reg1z@users.noreply.github.com>
2025-06-14 22:00:43 -0700

    added new directory imports

--- scripts/import-configs.sh
+++ scripts/import-configs.sh
@@ -5,6 +5,7 @@ cp -rf --remove-destination ../.config/hypr/* ~/.config/hypr
 cp -rf --remove-destination ../.config/waybar/* ~/.config/waybar
 cp -rf --remove-destination ../.config/rofi/* ~/.config/rofi
 cp -rf --remove-destination ../.config/mako/* ~/.config/mako
+cp -rf --remove-destination ../.config/kitty/* ~/.config/kitty
 cp -f  ../home/.* ~/
 
 # Copy wallpapers to ~/Pictures/wallpapers/
