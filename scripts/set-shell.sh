e71441642772109094bd41897fa34ae157f4675b
reg1z <21206554+reg1z@users.noreply.github.com>
2025-06-21 13:06:11 -0700

    Script: user selection of preferred shell

---
+++ scripts/set-shell.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPTS="scripts"
+USER_HOME=$(eval echo "~${SUDO_USER:-$USER}")
+CURRENT_USER=$(whoami)
+
+chosen_shell=$(gum choose "bash" "zsh" "fish" "xonsh" --header="Please select your preferred shell (default: bash):")
+
+if [ -n "$chosen_shell" ]; then
+  case $chosen_shell in
+    bash)
+      shell_path="/bin/bash"
+      ;;
+    zsh)
+      sudo pacman -S --needed zsh
+      shell_path="/usr/bin/zsh"
+      ;;
+    fish)
+      sudo pacman -S --needed fish
+      shell_path="/usr/bin/fish"
+      ;;
+    xonsh)
+      sudo pacman -S --needed xonsh
+      shell_path="/usr/bin/xonsh"
+      ;;
+  esac
+  
+  sudo chsh -s "$shell_path" "$CURRENT_USER"
+  echo "Default shell set to $shell_path for user $CURRENT_USER. You may need to log out and log back in for changes to take effect."
+else
+  echo "No shell selected; no changes made."
+fi
+
+
