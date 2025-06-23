47cfcfac0bd8c38bb937e6e6cf10c40afb1bb2b3
reg1z <21206554+reg1z@users.noreply.github.com>
2025-06-23 06:50:20 -0700

    Keybind updates

--- scripts/import-keybinds.sh
+++ scripts/import-keybinds.sh
@@ -5,4 +5,4 @@ USER_HOME=$(eval echo "~${SUDO_USER:-$USER}")
 CURRENT_USER=$(whoami)
 
 # Import kb configs only
-cp -rf --remove-destination .config/hypr/conf/keybindings/* $USER_HOME/.config/hypr/conf/keybindings/*
+cp -rf -v .config/hypr/conf/keybindings $USER_HOME/.config/hypr/conf
