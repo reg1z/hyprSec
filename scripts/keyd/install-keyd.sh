2b36dbb71104f81fb12e85f4a203ded705af2d2a
reg1z <21206554+reg1z@users.noreply.github.com>
2025-06-25 11:44:49 -0700

    Fixes to enthium v9

--- scripts/keyd/install-keyd.sh
+++ scripts/keyd/install-keyd.sh
@@ -4,7 +4,7 @@ SCRIPTS="scripts"
 USER_HOME=$(eval echo "~${SUDO_USER:-$USER}")
 CURRENT_USER=$(whoami)
 
-sudo pacman -S --needed keyd                                            # install keyd if not present already
-sudo cp -i scripts/keyd/laptop_layout.conf /etc/keyd/laptop_layout.conf # place KB config into correct directory
+sudo pacman -S --needed keyd                                             # install keyd if not present already
+sudo cp -i $SCRIPTS/keyd/laptop_layout.conf /etc/keyd/laptop_layout.conf # place KB config into correct directory
 sudo systemctl enable keyd
 sudo systemctl start keyd
