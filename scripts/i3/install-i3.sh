d44d88dc235885bb222907dd9ae8cbfd9075150c
reg1z <21206554+reg1z@users.noreply.github.com>
2025-06-24 20:19:50 -0700

    Added feh for configuring i3 desktop wallpaper

--- scripts/i3/install-i3.sh
+++ scripts/i3/install-i3.sh
@@ -4,5 +4,5 @@ SCRIPTS="scripts"
 USER_HOME=$(eval echo "~${SUDO_USER:-$USER}")
 CURRENT_USER=$(whoami)
 
-sudo pacman -S --needed --noconfirm i3-wm i3status i3lock autotiling xorg-xsetroot
+sudo pacman -S --needed --noconfirm i3-wm i3status i3lock autotiling xorg-xsetroot feh
 source $SCRIPTS/i3/import-i3-configs.sh
