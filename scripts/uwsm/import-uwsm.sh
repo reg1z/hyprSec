cad7e2155e87102477b091fc521e5606c7b39a2c
reg1z <21206554+reg1z@users.noreply.github.com>
2025-06-28 12:22:30 -0700

    fix

--- scripts/uwsm/import-uwsm.sh
+++ scripts/uwsm/import-uwsm.sh
@@ -4,9 +4,11 @@ SCRIPTS="scripts"
 USER_HOME=$(eval echo "~${SUDO_USER:-$USER}")
 CURRENT_USER=$(whoami)
 
-cp -f .config/uwsm/env $USER_HOME/.config/uwsm
-cp -f .config/uwsm/env-hyprland $USER_HOME/.config/uwsm
+mkdir -p $USER_HOME/.config/uwsm
 
-if gum confirm --default=false "Are you using an Nvidia graphics card?\n\nAnswering yes will set Nvidia-optimized environment variables."; then
+cp -f .config/uwsm/env $USER_HOME/.config/uwsm/
+cp -f .config/uwsm/env-hyprland $USER_HOME/.config/uwsm/
+
+if gum confirm --default=false "Are you using an Nvidia graphics card? Answering yes will set Nvidia-optimized environment variables."; then
   cp -f .config/uwsm/env_nvidia $USER_HOME/.config/uwsm/env
 fi
