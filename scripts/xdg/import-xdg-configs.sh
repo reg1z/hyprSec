1d6edb5706f7877f04641d06c4f7b1a8f9739ecd
reg1z <21206554+reg1z@users.noreply.github.com>
2025-06-28 18:44:07 -0700

    Attempts at Qt/KDE kvantum theming fixes

--- scripts/xdg/import-xdg-configs.sh
+++ scripts/xdg/import-xdg-configs.sh
@@ -5,6 +5,8 @@ USER_HOME=$(eval echo "~${SUDO_USER:-$USER}")
 CURRENT_USER=$(whoami)
 
 mkdir -p $USER_HOME/.config/xdg-desktop-portal
+sudo mkdir -p $USER_HOME/.config/xdg/menus
+sudo chmod 755 $USER_HOME/.config/xdg/menus
 
 cp -rf --remove-destination .config/xdg-desktop-portal/* $USER_HOME/.config/xdg-desktop-portal
-sudo cp -f $SCRIPTS/xdg/applications.menu /etc/xdg/menus # /etc/xdg/menus/applications.menu is necessary for applications to populate the app chooser in KDE apps like dolphin
+sudo cp -f $SCRIPTS/xdg/applications.menu /etc/xdg/menus/applications.menu # /etc/xdg/menus/applications.menu is necessary for applications to populate the app chooser in KDE apps like dolphin
